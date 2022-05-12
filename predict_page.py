343bb6dedb23ac2562a1a5bb8535596316d3ea90
nyura <11047766+nyurasul@users.noreply.github.com>
2022-05-12 03:42:27 -0700

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -4,7 +4,6 @@ import pickle
 import numpy as np
 import pandas as pd
 
-!{sys.executable} -m pip install sklearn
 def load_model():
     with open('saved_steps.pkl', 'rb') as file:
         data = pickle.load(file)
