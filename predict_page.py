34befce2a780ae3584775961ce81178185a6b75f
nyura <11047766+nyurasul@users.noreply.github.com>
2022-05-12 04:38:16 -0700

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -1,13 +1,12 @@
 import sys
-from joblib import load
 import streamlit as st
 import pickle
 import numpy as np
 import pandas as pd
 
 def load_model():
-    with open('saved_steps.pkl', 'rb') as file:
-        data = load(file)
+    with open('nyurasul\saved_steps.pkl', 'rb') as file:
+        data = pickle.load(file)
     return data
 
 data = load_model()
